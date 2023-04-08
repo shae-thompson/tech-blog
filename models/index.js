7900b5a1f61e435a64411a59b1157ec10f6f1eb7
mushroom <shaelah.mai@gmail.com>
2023-04-08 14:47:52 +1000

    models

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./user');
+const Comment = require('./comment');
+const Post = require('./post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post,{
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Comment, Post };
